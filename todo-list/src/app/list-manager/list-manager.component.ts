40efee828a59c4ab0f8ae03fd09a0ab27f19eac9
Pia Lyck Festersen <pialyck@gmail.com>
2018-03-10 14:22:27 +0100

    refactor app.component to use list-manager.component instead

--- todo-list/src/app/list-manager/list-manager.component.ts
+++ todo-list/src/app/list-manager/list-manager.component.ts
@@ -3,17 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'todo-list-manager',
   template: `
-    <p>
-      list-manager works!
-    </p>
+  <h1>
+    Welcome to {{ title }}!
+  </h1>
+  <todo-input (submit)="addItem($event)"></todo-input>
+  <ul>
+    <li *ngFor="let item of todoList">
+      <todo-item [todoItem]="item"></todo-item>
+    </li>
+  </ul>
   `,
   styleUrls: ['./list-manager.component.css']
 })
 export class ListManagerComponent implements OnInit {
+  title = 'NgGirls todo app';
+  public todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
+  addItem(title: string): void {
+    this.todoList.push({ title });
+  }
 }
