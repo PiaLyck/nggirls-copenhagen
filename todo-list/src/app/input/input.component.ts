98a66bb2cce513e666ddadee1bf6577b38975c3a
Pia Lyck Festersen <pialyck@gmail.com>
2018-03-10 11:26:13 +0100

    add todo-input component to app component

--- todo-list/src/app/input/input.component.ts
+++ todo-list/src/app/input/input.component.ts
@@ -3,14 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'todo-input',
   template: `
-    <p>
-      input works!
-    </p>
+  <input type="text">
+  <button>Save</button>
+  <p>The title is {{ title }}</p>
   `,
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
 
+  title = 'TITLE TITLE';
   constructor() { }
 
   ngOnInit() {
